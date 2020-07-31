bbb5299a8b09c6df0c247417654e509777a308e4
Ivan <proios@hotelnetsolutions.de>
2020-07-31 13:00:45 +0200

    wow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'doge';
+
+  muchWow() {
+    console.log("MUCH WOW");
+  }
 }
